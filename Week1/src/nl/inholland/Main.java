97dd500581a69d02ab601d4a31d93dba703e4724
Manoah <149895ja@gmail.com>
2020-09-04 09:33:44 +0200

    Assignment 1

---
+++ Week1/src/nl/inholland/Main.java
@@ -0,0 +1,61 @@
+package nl.inholland;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a scanner to read user input
+        Scanner in = new Scanner(System.in);
+
+        // Ask for group size and return the answer
+        System.out.print("How many students are in the group? ");
+        int size = Integer.parseInt(in.nextLine());
+        System.out.println("Group size: " + size);
+        System.out.println();
+
+        // Create Student array with given size
+        Student[] students = new Student[size];
+
+        // Fill array with student names
+        for (int i = 0; i < students.length; i++) {
+            System.out.printf("Please enter the name of student #%s and press [ENTER]: ", i + 1);
+            String name = in.nextLine();
+            students[i] = new Student(name);
+        }
+
+        System.out.println();
+
+        // Print the student names
+        for (int i = 0; i < students.length; i++) {
+            System.out.printf("Student #%s: %s", i + 1, students[i].name);
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Fill array with present value
+        for (int i = 0; i < students.length; i++) {
+            boolean validAnswer;
+            do {
+                System.out.printf("Is student #%s (%s) present? [Y/N + ENTER]: ", i + 1, students[i].name);
+                String answer = in.nextLine();
+                validAnswer = (answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("N"));
+                if (validAnswer) {
+                    students[i].present = answer.equalsIgnoreCase("Y");
+                } else {
+                    System.out.println("That was an invalid answer.");
+                }
+            }
+            while (!validAnswer);
+        }
+        
+        System.out.println();
+
+        // Show students and whether they're present
+        for (int i = 0; i < students.length; i++) {
+            System.out.printf("Student #%s: %-10s | Present: %s", i + 1, students[i].name, students[i].present);
+            System.out.println();
+        }
+    }
+}
---
+++ Week1/src/nl/inholland/Student.java
@@ -0,0 +1,10 @@
+package nl.inholland;
+
+public class Student {
+    String name;
+    boolean present;
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
