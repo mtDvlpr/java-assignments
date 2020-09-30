63b7c3a20b9bf30b10f3fffa5fb41252f6c51303
Manoah <149895ja@gmail.com>
2020-09-30 16:52:31 +0200

    Added login screen

---
+++ Week5/src/nl/inholland/App.java
@@ -0,0 +1,66 @@
+package nl.inholland;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        // Set Window properties
+        window.setHeight(200);
+        window.setWidth(300);
+        window.setTitle("Login Screen");
+
+        // Set grid
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(8);
+
+        // Create components
+        Label userLabel = new Label("Username:");
+        Label passwordLabel = new Label("Password:");
+        TextField userInput = new TextField();
+        PasswordField passwordInput = new PasswordField();
+        Button loginButton = new Button("Log in");
+
+        // Add attributes
+        userInput.setPromptText("Enter your username...");
+        passwordInput.setPromptText("Enter your password...");
+        loginButton.setDefaultButton(true);
+
+        // When button is clicked
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // TODO: Check if username/password are correct
+                new MainWindow();
+            }
+        });
+
+        // Add components to grid
+        gridPane.add(userLabel, 0,0);
+        gridPane.add(passwordLabel, 0, 1);
+        gridPane.add(userInput, 1, 0);
+        gridPane.add(passwordInput, 1, 1);
+        gridPane.add(loginButton, 1, 2);
+
+        // Set scene
+        window.setScene(new Scene(gridPane));
+
+        // Show window
+        window.show();
+    }
+}
---
+++ Week5/src/nl/inholland/MainWindow.java
@@ -0,0 +1,13 @@
+package nl.inholland;
+
+import javafx.stage.Stage;
+
+public class MainWindow {
+    Stage window;
+
+    public MainWindow() {
+        window = new Stage();
+
+        window.show();
+    }
+}
