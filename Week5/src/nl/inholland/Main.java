be391300c393abfe90b2b2f0ad6718c77b7ad689
Manoah <149895ja@gmail.com>
2020-09-30 16:30:41 +0200

    Added javaFX to week5

--- Week5/src/nl/inholland/Main.java
+++ Week5/src/nl/inholland/Main.java
@@ -1,8 +1,17 @@
 package nl.inholland;
 
-public class Main {
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        // some code
     }
 }
